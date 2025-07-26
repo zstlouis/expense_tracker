b35a4daab0e7a73e83d0da8c7644ca08ab1d020c
zstlouis <zstlouis7@gmail.com>
2025-07-26 12:06:54 -0400

    graph to flex inputted values

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,22 +1,30 @@
 import React from 'react'
-
+import 'boxicons'
 import {default as api} from '../store/apiSlice'
+
+
+
 export default function List() {
 
     const {data, isFetching, isSuccess, isError} = api.useGetLabelsQuery()
+    const [deleteTransaction] = api.useDeleteTransactionMutation()
 
-    let Transactions;
+    let Transactions
 
+    const handleClick = (e)=>{
+        if(!e.target.dataset.id) return 0
+        deleteTransaction({_id:e.target.dataset.id})
+    }
     if(isFetching){
         Transactions = <div>Fetching</div>
     }else if(isSuccess){
-        Transactions = data.map((v,i) => <Transaction key={i} category={v}></Transaction>)
+        Transactions = data.map((v,i) => <Transaction key={i} category={v} handler={handleClick}></Transaction>)
     }else if(isError){
         Transactions = <div>Error</div>
 
-    }else{
-        console.log("erro")
     }
+
+
 
     return (
         <div className='flex flex-col py-6 gap-3'>
@@ -27,12 +35,11 @@ export default function List() {
 }
 
 
-function Transaction({ category }) {
+function Transaction({ category, handler }) {
     if (!category) return null
-    console.log(category)
     return (
         <div className='item flex justify-center bg-gray-50 py-2 rounded-r' style={{ borderRight: `8px solid ${category.color}` }}>
-            <button className='px-3'><box-icon name='trash'></box-icon></button>
+            <button className='px-3' onClick={handler}><box-icon data-id={category._id??''} name='trash' ></box-icon></button>
             <span className='block w-full'>{category.name}</span>
         </div>
     )
